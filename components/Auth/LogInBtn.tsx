0fe7afcbf9e6d53b38f1960d1a78c0c9b3a00118
Jackoombs <jack.coombs@hotmail.co.uk>
2022-08-03 16:43:06 +0100

    Log in button now redirects to projects on sign in.

--- components/Auth/LogInBtn.tsx
+++ components/Auth/LogInBtn.tsx
@@ -1,17 +1,17 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const LogInBtn = () => {
-
-  const { data: session } = useSession()
+  const { data: session } = useSession();
 
   const handleClick = () => {
-    session ? signOut() : signIn()
-  }
+    session ? signOut() : signIn(undefined, { callbackUrl: "/projects" });
+  };
 
   return (
-    <button 
+    <button
       className="hidden justify-self-end text-slate-800 text-3xl font-medium bg-orange-200 w-max rounded px-4 py-1 duration-150 hover:bg-orange-500 hover:scale-105 lg:block"
-      onClick={handleClick}>
+      onClick={handleClick}
+    >
       {session ? "Log out" : "Log In"}
     </button>
   );
