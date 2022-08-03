375adcc909a3ab9a42a8aee56249570c05c36945
Jackoombs <jack.coombs@hotmail.co.uk>
2022-08-03 16:43:55 +0100

    Wrapped App in react query client provider.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+const queryClient = new QueryClient();
 
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>      
-      <Component {...pageProps} />    
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </SessionProvider>
-  )
+  );
 }
 
 export default MyApp;
