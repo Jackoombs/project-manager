2d6f12dea0ca11c3999812ddc136e47926c71e46
Jackoombs <jack.coombs@hotmail.co.uk>
2022-08-15 12:21:48 +0100

    Slight visual change.

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 const Project = ({ title, description, tasks, deadline }: Props) => {
   return (
-    <div className="flex flex-col gap-2 bg-gradient-to-tr from-orange-200 to-orange-500 rounded-xl text-slate-800 p-2">
+    <div className="flex flex-col gap-2 bg-gradient-to-tr from-orange-300 to-orange-600 rounded-xl text-slate-800 p-2">
       <div>
         <h3 className="flex gap-2 items-center text-3xl font-bold">
           <FaFlask />
