82173162b3caeb18f801eb2ed9936b6ea8054c91
Jackoombs <jack.coombs@hotmail.co.uk>
2022-08-20 08:46:58 +0100

    Added animations for the projects.

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -1,6 +1,9 @@
 import ProjectTasks from "./ProjectTasks";
 import ProjectDeadline from "./ProjectDeadline";
+import EditProject from "./EditProject";
+import { motion, AnimatePresence } from "framer-motion"
 import { FaFlask } from "react-icons/fa";
+import { useState } from "react";
 
 interface Props {
   title: string;
@@ -11,18 +14,52 @@ interface Props {
 }
 
 const Project = ({ title, description, tasks, deadline }: Props) => {
+
+  const [isOpen, setIsOpen] = useState(false)
+  const handleClick = () => setIsOpen(!isOpen)
+
+  const titlePosition = () => {
+    return isOpen ? "items-start" : "items-center"
+  }
+
   return (
-    <div className="flex flex-col gap-2 bg-gradient-to-tr from-orange-300 to-orange-600 rounded-xl text-slate-800 p-2">
-      <div>
-        <h3 className="flex gap-2 items-center text-3xl font-bold">
+    <motion.div 
+      initial={false}
+      onClick={handleClick}
+      whileHover={{
+        outline: isOpen ? "" : "2px solid #fed7aa",
+        transition: { type: "linear", duration: 0.1 },
+      }}
+      className="relative flex flex-col gap-2 bg-gradient-to-tr from-orange-400 to-orange-600 rounded text-slate-800 p-2">
+      <motion.div 
+        layout
+        className={`${titlePosition()} flex flex-col`}>
+        <motion.h3 layout className="flex gap-2 items-center text-xl font-bold">
           <FaFlask />
           {title}
-        </h3>
-        <p className="text-lg">{description}</p>
-      </div>
-      <ProjectTasks tasks={tasks} />
-      {deadline && <ProjectDeadline deadline={deadline} />}
-    </div>
+        </motion.h3>
+        <motion.p layout className="text-md">{description}</motion.p>
+      </motion.div>
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.div
+            key="content"
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: "auto"},
+              collapsed: { opacity: 0, height: 0}
+            }}
+            transition={{ duration: 0.2 }}
+            >
+            <ProjectTasks tasks={tasks} />
+            {deadline && <ProjectDeadline deadline={deadline} />}
+            <EditProject setIsOpen={setIsOpen} />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
   );
 };
 
